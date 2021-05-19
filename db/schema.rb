2d9023e381ac9bc9a751cbd3412f9e4e92d269f9
Rafael Mello <rafael.ferreira.sobral@gmail.com>
2021-05-19 14:32:38 +1000

    Devise installed, User Model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_013320) do
+ActiveRecord::Schema.define(version: 2021_05_19_034650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.integer "phone"
-    t.string "unit_number"
-    t.string "street_number"
-    t.string "street_name"
-    t.string "suburb"
-    t.string "state"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end

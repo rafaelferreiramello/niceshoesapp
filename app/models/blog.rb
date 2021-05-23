be9660fee440650ebe110099c6cd0fd2ef566319
Rafael Mello <rafael.ferreira.sobral@gmail.com>
2021-05-24 00:42:52 +1000

    Shoes index page refactored

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,8 @@ class Blog < ApplicationRecord
 
     has_one_attached :cover
 
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :post, presence: true
+
 end
